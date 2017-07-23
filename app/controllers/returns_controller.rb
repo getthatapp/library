ed4584420ede10420bc4cab1a10c8b23c9e5191b
Grzegorz <apps@getthatapp.pl>
2017-07-23 14:29:48 +0200

    Added returning ability

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -2,8 +2,12 @@ class ReturnsController < ApplicationController
   def update
     book = Book.find(params[:book_id])
     order = book.orders.find_by(returned_at: nil)
-    order.update(returned_at: DateTime.now)
-    redirect_to book_path(book)
+    if order.user == current_user
+      order.update(returned_at: DateTime.now)
+      redirect_to book_path(book), success: "Thank you for returning this book"
+    else
+      redirect_to book_path(book), danger: "You can't return this book. It's borrowed by someone else!"
+    end
   end
 
 end
