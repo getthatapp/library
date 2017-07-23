6215701331b7dfb7d39704c2f491b0c7622bd3d4
Grzegorz <apps@getthatapp.pl>
2017-07-23 19:46:10 +0200

    Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {ssl: { 
-        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-        ca_path: "/etc/ssl/certs"}}}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
 end
